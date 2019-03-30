5576673e63c0957b777ee5633a86f325fc0f898b
Geonu <geoun1109@gmail.com>
2019-03-30 19:14:50 +0900

    initial commit

---
+++ CafeO/Source/Check/CheckVC.swift
@@ -0,0 +1,30 @@
+//
+//  CheckVC.swift
+//  CafeO
+//
+//  Created by 전건우 on 30/03/2019.
+//  Copyright © 2019 geonu. All rights reserved.
+//
+
+import UIKit
+
+class CheckVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
